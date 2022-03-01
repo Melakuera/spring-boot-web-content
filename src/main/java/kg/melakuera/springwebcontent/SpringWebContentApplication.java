560f64e12d7240b1fbbdf85ec17348571261d3af
Eld Musabaev <musabaeveldiar@gmail.com>
2022-03-01 17:28:06 +0600

    init

---
+++ src/main/java/kg/melakuera/springwebcontent/SpringWebContentApplication.java
@@ -0,0 +1,14 @@
+package kg.melakuera.springwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebContentApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
