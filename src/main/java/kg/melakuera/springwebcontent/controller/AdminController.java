41559662bd04f9f9d7b4d9a2de07c545dd431f63
Eld Musabaev <musabaeveldiar@gmail.com>
2022-03-22 16:30:25 +0600

    Валидация на странице админки 2

--- src/main/java/kg/melakuera/springwebcontent/controller/AdminController.java
+++ src/main/java/kg/melakuera/springwebcontent/controller/AdminController.java
@@ -8,10 +8,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @AllArgsConstructor
@@ -21,26 +24,39 @@ public class AdminController {
     private final AppUserService appUserService;
 
     @GetMapping("/users")
-    public String getAll(@AuthenticationPrincipal AppUser appUser, Model model) {
+    public String getAll(@AuthenticationPrincipal AppUser authUser, Model model) {
         model.addAttribute("users", appUserService.findAll());
-        model.addAttribute("user", appUser);
+        model.addAttribute("user", authUser);
 
         return "users";
     }
 
     @GetMapping("/user/{id}")
-    public String getOne(@AuthenticationPrincipal AppUser appUser,
-                         @PathVariable Long id,
-                         Model model) {
-        model.addAttribute("foundUser", appUserService.findById(id));
+    public String getOne(
+            @AuthenticationPrincipal AppUser authUser,
+            @PathVariable("id") Long id,
+            Model model) {
+        model.addAttribute("user", authUser);
+        model.addAttribute("appUser", appUserService.findById(id));
         model.addAttribute("roles", Role.values());
-        model.addAttribute("user", appUser);
 
         return "userEdit";
     }
 
     @PostMapping("/user/{id}")
-    public String update(@ModelAttribute("user") AppUser appUser, @PathVariable Long id) {
+    public String update(
+            @AuthenticationPrincipal AppUser authUser,
+            @PathVariable("id") Long id,
+            @ModelAttribute("appUser") @Valid AppUser appUser,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("user", authUser);
+            model.addAttribute("roles", Role.values());
+
+            return "userEdit";
+        }
         appUserService.updateByAdmin(appUser, id);
 
         return "redirect:/users";
