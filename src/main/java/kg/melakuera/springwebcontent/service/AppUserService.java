d9707448ef710ef70a9becef6dd9298d5ee68bf2
Eld Musabaev <musabaeveldiar@gmail.com>
2022-03-27 20:00:40 +0600

    чекпоинт

--- src/main/java/kg/melakuera/springwebcontent/service/AppUserService.java
+++ src/main/java/kg/melakuera/springwebcontent/service/AppUserService.java
@@ -21,91 +21,93 @@ import java.util.UUID;
 @Log
 public class AppUserService implements UserDetailsService {
 
-	private final AppUserRepository appUserRepository;
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	private final AppMailSenderService appMailSenderService;
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AppMailSenderService appMailSenderService;
 
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		return appUserRepository.findByEmail(email)
-				.orElseThrow(() -> new UsernameNotFoundException(
-						String.format("Пользователь с данной %s эл. почтой не найден", email)));
-	}
-
-	public boolean register(AppUser appUser) {
-		boolean user =  appUserRepository.findByEmail(appUser.getEmail()).isPresent();
-		if (user) {
-
-			log.info("Такой пользователь уже сущесвует!");
-			return false;
-		}
-
-		appUser.setPassword(bCryptPasswordEncoder.encode(appUser.getPassword()));
-		appUserRepository.save(appUser);
-
-		return true;
-	}
-
-	public List<AppUser> findAll() {
-		return appUserRepository.findAll();
-	}
-
-	public AppUser findById(Long id) {
-		return appUserRepository.findById(id).orElse(null);
-	}
-
-	public boolean isAdmin(AppUser appUser){
-		return appUser.getRole().equals(Role.ROLE_ADMIN);
-	}
-
-	public AppUser findByResetPasswordCode(String code) {
-		return appUserRepository.findByResetPasswordCode(code).orElse(null);
-	}
-
-
-	public void update(AppUser appUser, Long id) {
-		AppUser updatedAppUser = appUserRepository.findById(id).orElse(null);
-		assert updatedAppUser != null;
-		updatedAppUser.setFirstName(appUser.getFirstName());
-		updatedAppUser.setLastName(appUser.getLastName());
-	}
-
-	public void updateByAdmin(AppUser appUser, Long id) {
-		AppUser updatingAppUser = appUserRepository.findById(id).orElse(null);
-		assert updatingAppUser != null;
-		updatingAppUser.setFirstName(appUser.getFirstName());
-		updatingAppUser.setLastName(appUser.getLastName());
-		updatingAppUser.setRole(appUser.getRole());
-		System.out.println(appUser.getEmail());
-		System.out.println(appUser.getPassword());
-	}
-
-	public void resetPassword(Long id) {
-		AppUser appUser = appUserRepository.findById(id).orElse(null);
-		String code = UUID.randomUUID().toString();
-		assert appUser != null;
-		appUser.setResetPasswordCode(code);
-		Thread thread = new Thread(() -> appMailSenderService.sendResetPassword(appUser));
-		thread.start();
-
-		log.info("Запрос на сброс пароля отправил пользователь с id: " + id);
-	}
-
-	public void updatePassword(AppUser appUser, String pw1, String pw2) {
-		appUser.setPassword(bCryptPasswordEncoder.encode(pw1));
-
-		log.info(String.format("Пароль пользователя %s успешно изменён", appUser.getEmail()));
-	}
-
-    public void unsubscribe(AppUser appUser, Long id) {
-		AppUser foundUser = appUserRepository.findById(id).orElse(null);
-		assert foundUser != null;
-		foundUser.getSubscribers().remove(appUser);
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("Пользователь с данной %s эл. почтой не найден", email)));
     }
 
-	public void subscribe(AppUser appUser, Long id) {
-		AppUser foundUser = appUserRepository.findById(id).orElse(null);
-		assert foundUser != null;
-		foundUser.getSubscribers().add(appUser);
-	}
+    public boolean register(AppUser appUser) {
+        boolean user = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if (user) {
+
+            log.info("Такой пользователь уже сущесвует!");
+            return false;
+        }
+
+        appUser.setPassword(bCryptPasswordEncoder.encode(appUser.getPassword()));
+        appUserRepository.save(appUser);
+
+        return true;
+    }
+
+    public List<AppUser> findAll() {
+        return appUserRepository.findAll();
+    }
+
+    public AppUser findById(Long id) {
+        return appUserRepository.findById(id).orElse(null);
+    }
+
+    public AppUser findByEmail(String email) {
+        return appUserRepository.findByEmail(email).orElse(null);
+    }
+
+    public boolean isAdmin(AppUser appUser) {
+        return appUser.getRole().equals(Role.ROLE_ADMIN);
+    }
+
+    public AppUser findByResetPasswordCode(String code) {
+        return appUserRepository.findByResetPasswordCode(code).orElse(null);
+    }
+
+
+    public void update(AppUser appUser, Long id) {
+        AppUser updatedAppUser = appUserRepository.findById(id).orElse(null);
+        assert updatedAppUser != null;
+        updatedAppUser.setFirstName(appUser.getFirstName());
+        updatedAppUser.setLastName(appUser.getLastName());
+    }
+
+    public void updateByAdmin(AppUser appUser, Long id) {
+        AppUser updatingAppUser = appUserRepository.findById(id).orElse(null);
+        assert updatingAppUser != null;
+        updatingAppUser.setFirstName(appUser.getFirstName());
+        updatingAppUser.setLastName(appUser.getLastName());
+        updatingAppUser.setRole(appUser.getRole());
+    }
+
+    public void resetPassword(Long id) {
+        AppUser appUser = appUserRepository.findById(id).orElse(null);
+        String code = UUID.randomUUID().toString();
+        assert appUser != null;
+        appUser.setResetPasswordCode(code);
+        Thread thread = new Thread(() -> appMailSenderService.sendResetPassword(appUser));
+        thread.start();
+
+        log.info("Запрос на сброс пароля отправил пользователь с id: " + id);
+    }
+
+    public void updatePassword(AppUser appUser, String pw1, String pw2) {
+        appUser.setPassword(bCryptPasswordEncoder.encode(pw1));
+
+        log.info(String.format("Пароль пользователя %s успешно изменён", appUser.getEmail()));
+    }
+
+    public void unsubscribe(AppUser appUser, Long id) {
+        AppUser foundUser = appUserRepository.findById(id).orElse(null);
+        assert foundUser != null;
+        foundUser.getSubscribers().remove(appUser);
+    }
+
+    public void subscribe(AppUser appUser, Long id) {
+        AppUser foundUser = appUserRepository.findById(id).orElse(null);
+        assert foundUser != null;
+        foundUser.getSubscribers().add(appUser);
+    }
 }
--- src/test/java/kg/melakuera/springwebcontent/service/AppUserServiceTest.java
+++ src/test/java/kg/melakuera/springwebcontent/service/AppUserServiceTest.java
@@ -3,11 +3,7 @@ package kg.melakuera.springwebcontent.service;
 import kg.melakuera.springwebcontent.entity.AppUser;
 import kg.melakuera.springwebcontent.entity.Role;
 import kg.melakuera.springwebcontent.repository.AppUserRepository;
-import org.assertj.core.api.Assertions;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,6 +11,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class AppUserServiceTest {
@@ -29,44 +29,104 @@ public class AppUserServiceTest {
     private AppUserService appUserService;
 
     @Test
-    void registerShouldBeSuccessfully() {
-        // Mockito
-        AppUser expectedAppUser = new AppUser(
-                1L, "Tom", "Bob", "email@email.com", "1", null, null);
-
-        Mockito.when(appUserRepository.findById(1L)).thenReturn(Optional.of(expectedAppUser));
-        AppUser actualAppUser = appUserService.findById(1L);
-
-        Assertions.assertThat(appUserService.findById(1L)).isEqualTo(actualAppUser);
-    }
-    @Test
     void isAdminShouldBeTrue() {
-        // Hamcrest
+
         AppUser appUser = new AppUser();
         appUser.setRole(Role.ROLE_ADMIN);
 
         boolean res = appUserService.isAdmin(appUser);
 
-        MatcherAssert.assertThat(res, Matchers.equalTo(true));
+        assertThat(res).isTrue();
     }
 
     @Test
     void isAdminShouldBeFalse() {
-        // Hamcrest
+
         AppUser appUser = new AppUser();
         appUser.setRole(Role.ROLE_USER);
 
         boolean res = appUserService.isAdmin(appUser);
 
-        MatcherAssert.assertThat(res, Matchers.equalTo(false));
+        assertThat(res).isFalse();
     }
 
     @Test
     void shouldThrowExpLoadUserByUsername() {
-        // AssertJ
-        Assertions.assertThatThrownBy(() ->
-                        appUserService.loadUserByUsername("test@test.com"))
+
+        assertThatThrownBy(() ->
+                appUserService.loadUserByUsername("test@test.com"))
                 .isInstanceOf(UsernameNotFoundException.class)
                 .hasMessage("Пользователь с данной test@test.com эл. почтой не найден");
+    }
+
+    @Test
+    void shouldFindUserById() {
+
+        AppUser expectedAppUser = new AppUser(
+                1L, "Tom", "Bob", "email@email.com", "1", null, null);
+
+        when(appUserRepository.findById(1L)).thenReturn(Optional.of(expectedAppUser));
+        AppUser actualAppUser = appUserService.findById(1L);
+
+        assertThat(appUserService.findById(1L)).isEqualTo(actualAppUser);
+    }
+
+    @Test
+    void shouldRegisterUser() {
+
+        AppUser appUser = new AppUser();
+        appUser.setEmail("email@email.com");
+        appUser.setPassword("1111");
+        appUserService.register(appUser);
+
+        verify(passwordEncoder, times(1)).encode(anyString());
+        verify(appUserRepository, times(1)).findByEmail(eq(appUser.getEmail()));
+        verify(appUserRepository, times(1)).save(any(AppUser.class));
+    }
+
+
+    @Test
+    void shouldUpdateUser() {
+        AppUser appUserFromForm = new AppUser();
+        appUserFromForm.setFirstName("Tom");
+        appUserFromForm.setLastName("Henson");
+
+        AppUser updatedAppUser = new AppUser();
+        updatedAppUser.setFirstName("Bob");
+        updatedAppUser.setLastName("Henson");
+
+        when(appUserRepository.findById(anyLong())).thenReturn(Optional.of(updatedAppUser));
+
+        appUserService.update(appUserFromForm, anyLong());
+        assertThat(updatedAppUser).isEqualToComparingFieldByField(appUserFromForm);
+    }
+
+    @Test
+    void shouldReturnFalse() {
+
+        // Пользователь извне
+        AppUser appUser = new AppUser();
+        appUser.setEmail("email@email.com");
+        // Пользователь который из формы
+        AppUser AppUserFromForm = new AppUser();
+        AppUserFromForm.setEmail("email@email.com");
+        AppUserFromForm.setPassword("1111");
+        /*
+        Эмулируем поведение метода findByEmail(String email),
+        так чтобы при передаче эл. почты юзера из формы вернулся юзер извне,
+        что означает, что такой пользователь уже существует в базе данных
+         */
+        when(appUserRepository.findByEmail(AppUserFromForm.getEmail()))
+                .thenReturn(Optional.of(appUser));
+        // Executing...
+        boolean res = appUserService.register(AppUserFromForm);
+        // Утверждаем, что результат метода register(AppUser appUser) будет false
+        assertThat(res).isFalse();
+        // посему passwordEncoder.encode(String rawPassword) выполнится 0 раз
+        verify(passwordEncoder, times(0)).encode(anyString());
+        // и пароль юзера из формы не зашифрован
+        assertThat(AppUserFromForm.getPassword()).doesNotContain("$2a$");
+        // А юзер из формы не сохранится в бд
+        verify(appUserRepository, times(0)).save(any(AppUser.class));
     }
 }
